340b5a207b67b3f7f072014d54e6dc569d905020
Manoj-MCA <manojsharma00018@gmail.com>
2022-09-18 18:28:52 +0530

    Added Monitoring Page

---
+++ EMS.Core.Entities/Survey/Monitoring.cs
@@ -0,0 +1,24 @@
+﻿using EMS.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Core.Entities.Survey
+{
+    [Table("Monitoring", Schema = "Survey")]
+    public class Monitoring:Base<int>
+    {
+        public string SeasonName { get; set; }
+        public string ZoneName { get; set; }
+        public string EventName { get; set; }
+        public string Percentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Comment { get; set; }
+        public int DetailsId { get; set; }
+
+    }
+}
---
+++ EMS/Controllers/Survey/MonitoringController.cs
@@ -0,0 +1,48 @@
+﻿using EMS.Core.Entities.Survey;
+using EMS.Core.Services.GenericService;
+using EMS.Core.ViewModelEntitity.Survey;
+using EMS.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMS.Controllers.Survey
+{
+    public class MonitoringController : Controller
+    {
+        private readonly IGenericService<Monitoring, int> _IMonitoringService;
+
+        public MonitoringController(IGenericService<Monitoring, int> monitoringService)
+        {
+            _IMonitoringService = monitoringService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var response = await _IMonitoringService.GetList(x => !x.IsDeleted);
+            return View(ViewHelpers.GetViewName("Survey", "Monitoring"), response);
+        }
+
+        public async Task<IActionResult> UpdateTaskStatus(int taskId, string taskName, string status)
+        {
+            var model = new UpdateTaskVm()
+            {
+                TaskId = taskId,
+                PreviousStatus = status,
+                TaskName = taskName
+            };
+
+            return await Task.Run(() => PartialView(ViewHelpers.GetViewName("Survey", "TaskManagerUpdatePartial"), model));
+        }
+        [HttpPost]
+        public async Task<IActionResult> PostUpdateTaskStatus(UpdateTaskVm model)
+        {
+            var taskModel = await _IMonitoringService.GetSingle(x => x.Id == model.TaskId);            
+            taskModel.Comment = model.Comment;
+
+            var response = await _IMonitoringService.UpdateEntity(taskModel);
+            return Json(ResponseHelper.ResponseMessage(response, Core.Entities.Common.OperationType.Update));
+        }
+    }
+}
